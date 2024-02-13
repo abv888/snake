04a5aa4d4de54f8e772417efa618a32974d23268
Bagrat Arutyunov <arutyunovbagrat@gmail.com>
2024-02-13 13:15:57 +0300

    create snake body

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ screen.setup(width=1200, height=1200)
 screen.bgcolor("black")
 screen.title("Snake")
 
-
+for i in range(3):
+    segment = Turtle("square")
+    segment.color("white")
+    segment.goto(x=-20 * i, y=0)
 
 screen.exitonclick()
